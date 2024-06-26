7267cbff36b7ce04f88073b173b22e119b1a7627
Amansimlembe <168660420+Amansimlembe@users.noreply.github.com>
2024-06-26 20:44:02 +0300

    Add files via upload

--- gian.js
+++ gian.js
@@ -1,10 +1,13 @@
-/*function openMenu() {
-    var x = document.getElementById("sidenav");
-    x.style.display = "block";
- }
- function closeMenu() {
-     var x = document.getElementById("sidenav");
-     x.style.display = "none";
- }
- javascript linking has shida....am tired.
- */
+function openMenu() {
+    document.getElementById("sidenav").style.display = "flex";
+}
+
+function closeMenu() {
+    document.getElementById("sidenav").style.display = "none";
+}
+
+// Toggle content visibility in sidebar for small screens
+function toggleSidebarContent(id) {
+    var content = document.getElementById(id);
+    content.classList.toggle("visible");
+}
